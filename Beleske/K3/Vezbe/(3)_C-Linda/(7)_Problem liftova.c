6842bd5c0758c86ecb2777ae53cf6867928f92b3
Pavle Sarenac <sp200359d@student.etf.bg.ac.rs>
2023-08-30 18:21:38 +0200

    Used mutex in elevator problem instead of cleaning up the tuple space.

--- Beleske/K3/Vezbe/(3)_C-Linda/(7)_Problem liftova.c
+++ Beleske/K3/Vezbe/(3)_C-Linda/(7)_Problem liftova.c
@@ -2,10 +2,9 @@
 // obavezno izadju. Nakon izlaska putnika svi putnici koji su cekali na ulazak udju u lift i kazu na koji sprat zele da predju.
 // Tek kada se svi izjasne lift ide dalje. Nije potrebno optimizovati put lifta i putnika.
 
-// U funkciji passenger linije koda inp("floorNumber", enteringFloor); i inp("floorNumber", leavingFloor); su vrlo vazne jer je
-// moguce da se dogodi da istovremeno neko 2 putnika pozovu lift npr. jedan da dodje do sprata 5 da bi usao, a drugi da
-// ode do sprata 5 da bi izasao i onda bismo u prostoru torki imali duplikat iste torke i zato cistimo te duplikate pomocu inp.
-// Kada ne bismo to radili, program bi vrlo lako mogao da zavrsi u nekonzistentnom stanju.
+// Za svaki sprat postoji po jedan mutex - ("mutexFloorNumber", brojSprata) da ne bi moglo da se dogodi da neko npr. pozove lift
+// da dodje do 5. sprata da bi on tu usao u lift, a da isto tako neko klikne 5. sprat u liftu da bi izasao na 5. spratu i da onda
+// u prostoru torki imamo dve iste torke sto bi onda dovelo sistem u nekonzistentno stanje.
 const int NUMBER_OF_FLOORS = ...;
 const int NUMBER_OF_PASSENGERS = ...;
 
@@ -13,6 +12,7 @@ void initialize() {
     for (int i = 0; i < NUMBER_OF_FLOORS; i++) {
         out("numberOfPeopleLeftInElevator", i, 0);
         out("numberOfPeopleLeftOutsideOfElevator", i, 0);
+        out("mutexFloorNumber", i);
     }
     eval(elevator());
     for (int i = 0; i < NUMBER_OF_PASSENGERS; i++) {
@@ -37,7 +37,9 @@ void passenger(int id, int enteringFloor, int leavingFloor) {
         // Putnik se prijavljuje da ceka lift i ako je prvi na spratu enteringFloor koji je dosao, treba i da pozove lift ukoliko
         // ga nije niko vec pozvao da dodje do sprata enteringFloor.
         in("numberOfPeopleLeftOutsideOfElevator", enteringFloor, ?numberOfPeopleLeftOutsideOfElevator);
+        in("mutexFloorNumber", enteringFloor);
         if (numberOfPeopleLeftOutsideOfElevator == 0 && !rdp("floorNumber", enteringFloor)) out("floorNumber", enteringFloor);
+        out("mutexFloorNumber", enteringFloor);
         out("numberOfPeopleLeftOutsideOfElevator", enteringFloor, numberOfPeopleLeftOutsideOfElevator + 1);
 
         // Putnik ulazi u lift i klikce u liftu sprat na koji zeli da ide (leavingFloor) pod uslovom da je lift prazan i da
@@ -45,15 +47,15 @@ void passenger(int id, int enteringFloor, int leavingFloor) {
         in("enterElevator", enteringFloor);
         in("numberOfPeopleLeftOutsideOfElevator", enteringFloor, ?numberOfPeopleLeftOutsideOfElevator);
         in("numberOfPeopleLeftInElevator", leavingFloor, ?numberOfPeopleLeftInElevator);
+        in("mutexFloorNumber", leavingFloor);
         if (numberOfPeopleLeftInElevator == 0 && !rdp("floorNumber", leavingFloor)) out("floorNumber", leavingFloor);
-        inp("floorNumber", enteringFloor);
+        out("mutexFloorNumber", leavingFloor);
         out("numberOfPeopleLeftOutsideOfElevator", enteringFloor, numberOfPeopleLeftOutsideOfElevator - 1);
         out("numberOfPeopleLeftInElevator", leavingFloor, numberOfPeopleLeftInElevator + 1);
         out("enterElevator", enteringFloor);
 
         // Putnik izlazi iz lifta.
         in("leaveElevator", leavingFloor);
-        inp("floorNumber", leavingFloor);
         in("numberOfPeopleLeftInElevator", leavingFloor, ?numberOfPeopleLeftInElevator);
         out("numberOfPeopleLeftInElevator", leavingFloor, numberOfPeopleLeftInElevator - 1);
         out("leaveElevator", leavingFloor);
